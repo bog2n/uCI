59ed3fce68ea9c795216fe965c1bdc04a263b76d
duckl1ng <bogdan@sanocki.info>
2024-07-16 19:53:16 +0200

    minor ui tweaks

--- pkg/webui.go
+++ pkg/webui.go
@@ -17,22 +17,37 @@ type link struct {
 	Href string
 }
 
-func (c *Config) UiHandler(w http.ResponseWriter, r *http.Request) {
-	p := page{
-		Header: "Hello there",
-		Nav: []link{
-			link{"test1", "/"},
-			link{"test2", "/"},
-			link{"test3", "/"},
-		},
-		Content: "hello!",
+func (c *Config) generateNav() (out []link) {
+	for _, repo := range c.Repositories {
+		n := repo.Name + "@" + repo.Branch
+		out = append(out, link{n, "repo?name=" + n})
 	}
+	return
+}
+
+func (c *Config) UiHandler(w http.ResponseWriter, r *http.Request) {
+	p := page{Nav: c.generateNav()}
 	switch r.URL.Path {
 	case "/":
+		p.Header = "easy way to deploy your code."
 		if err := tmpl.Execute(w, "index", p); err != nil {
 			errno := http.StatusInternalServerError
 			http.Error(w, http.StatusText(errno), errno)
 			log.Print(err)
+		}
+	case "/repo":
+
+		if repo, ok := c.Repos[r.FormValue("name")]; ok {
+			p.Header = repo.Name + "@" + repo.Branch
+			p.Content = repo
+			if err := tmpl.Execute(w, "repo", p); err != nil {
+				errno := http.StatusInternalServerError
+				http.Error(w, http.StatusText(errno), errno)
+				log.Print(err)
+			}
+		} else {
+			http.NotFound(w, r)
+			return
 		}
 	default:
 		http.NotFound(w, r)
